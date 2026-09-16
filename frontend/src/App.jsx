9f594bf386cdb95eaf216c74a94464c2c454d93d
Drishti <dlakhyani07@gmail.com>
2026-09-16 05:56:06 +0530

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://ephemeris-backend-bkf2.onrender.com";
 const UPLOAD_URL = `${API_BASE}/upload`;
 
 
